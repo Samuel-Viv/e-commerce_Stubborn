b4b0bd3c344df7629b43c25f711ecf5ea263492f
Samuel VIVIER <samvivier25@gmail.com>
2024-09-21 10:43:06 +0200

    Add modification fixture and homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,11 +12,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(ProductRepository $repository): Response
     {
-        $product1 = $repository->find(1);
-        $product2 = $repository->find(4);
-        $product3 = $repository->find(10);
+        $product1 = $repository->findOneBy(['name' => 'Blackbelt']);
+        $product2 = $repository->findOneBy(['name' => 'Pokeball']);
+        $product3 = $repository->findOneBy(['name' => 'BornInUsa']);
         return $this->render('home/index.html.twig', [
-           'products' =>[$product1,$product2, $product3],
+            'products' => [$product1, $product2, $product3],
         ]);
     }
 }
